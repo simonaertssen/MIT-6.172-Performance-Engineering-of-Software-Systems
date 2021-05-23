d4a554cbc6f7d5da2127bf408796aa5923153473
Simon Aertssen <Simonaertssen@gmail.com>
2021-05-23 21:05:23 +0200

    Making my way through the first week, easy

--- Week 1 Introduction/MIT6_172F18_hw1/c-primer/pointer.c
+++ Week 1 Introduction/MIT6_172F18_hw1/c-primer/pointer.c
@@ -4,44 +4,45 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-int main(int argc, char * argv[]) {  // What is the type of argv?
+int main(int argc, char* argv[]) {  // What is the type of argv? It is of type 'char'.
   int i = 5;
   // The & operator here gets the address of i and stores it into pi
-  int * pi = &i;
+
+  int* pi = &i;
   // The * operator here dereferences pi and stores the value -- 5 --
   // into j.
   int j = *pi;
 
   char c[] = "6.172";
-  char * pc = c;  // Valid assignment: c acts like a pointer to c[0] here.
+  char* pc = c;  // Valid assignment: c acts like a pointer to c[0] here.
   char d = *pc;
-  printf("char d = %c\n", d);  // What does this print?
+  printf("char d = %c\n", d);  // What does this print? The first value of c, "6".
 
   // compound types are read right to left in C.
   // pcp is a pointer to a pointer to a char, meaning that
   // pcp stores the address of a char pointer.
-  char ** pcp;
-  pcp = argv;  // Why is this assignment valid?
+  char** pcp;
+  pcp = argv;  // Why is this assignment valid? Because both are pointers.
 
-  const char * pcc = c;  // pcc is a pointer to char constant
-  char const * pcc2 = c;  // What is the type of pcc2?
+  const char* pcc = c;  // pcc is a pointer to char constant
+  char const* pcc2 = c;  // What is the type of pcc2? It is a constant pointer to a char.
 
+  // // For each of the following, why is the assignment:
+  // *pcc = '7';  // invalid? Because this is read-only variable (a constant) and not assignable.
+  pcc = *pcp;  // valid? Because the pointer to the value can change.
+  pcc = argv[0];  // valid? Because argv[0] is a pointer as well.
+
+  char* const cp = c;  // cp is a const pointer to char
   // For each of the following, why is the assignment:
-  *pcc = '7';  // invalid?
-  pcc = *pcp;  // valid?
-  pcc = argv[0];  // valid?
+  // cp = *pcp;  // invalid? Because cp is a const pointer and not a double pointer to char.
+  // cp = *argv;  // invalid? Same here.
+  *cp = '!';  // valid? Because only the pointer needs to remain constant.
 
-  char * const cp = c;  // cp is a const pointer to char
+  const char* const cpc = c;  // cpc is a const pointer to char const
   // For each of the following, why is the assignment:
-  cp = *pcp;  // invalid?
-  cp = *argv;  // invalid?
-  *cp = '!';  // valid?
-
-  const char * const cpc = c;  // cpc is a const pointer to char const
-  // For each of the following, why is the assignment:
-  cpc = *pcp;  // invalid?
-  cpc = argv[0];  // invalid?
-  *cpc = '@';  // invalid?
+  cpc = *pcp;  // invalid? Because nothing can be altered after declaration.
+  cpc = argv[0];  // invalid? Because nothing can be altered after declaration.
+  *cpc = '@';  // invalid? Because nothing can be altered after declaration.
 
   return 0;
 }

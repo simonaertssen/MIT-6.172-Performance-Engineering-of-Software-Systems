bb863ea58c7f88e5e5939f64f501fca35af757ff
Simon Aertssen <Simonaertssen@gmail.com>
2021-05-24 13:15:21 +0200

    Added a better structure and a page for writing comments

--- Week 1 Introduction/c-primer/sizes.c
+++ Week 1 Introduction/c-primer/sizes.c
@@ -17,34 +17,58 @@ int main() {
   // printf("size of %s : %zu bytes \n", "int", sizeof(int));
 
   PRINT_SIZE("int", sizeof(int));
+  PRINT_SIZE("int*", sizeof(int*));
   PRINT_SIZE("short", sizeof(short));
+  PRINT_SIZE("short*", sizeof(short*));
   PRINT_SIZE("long", sizeof(long));
+  PRINT_SIZE("long*", sizeof(long*));
   PRINT_SIZE("char", sizeof(char));
+  PRINT_SIZE("char*", sizeof(char*));
   PRINT_SIZE("float", sizeof(float));
+  PRINT_SIZE("float*", sizeof(float*));
   PRINT_SIZE("double", sizeof(double));
+  PRINT_SIZE("double*", sizeof(double*));
 
   PRINT_SIZE("unsigned int", sizeof(unsigned int));
+  PRINT_SIZE("unsigned int*", sizeof(unsigned int*));
   PRINT_SIZE("long long", sizeof(long long));
+  PRINT_SIZE("long long*", sizeof(long long*));
 
   PRINT_SIZE("int8_t", sizeof(int8_t));
+  PRINT_SIZE("int8_t*", sizeof(int8_t*));
   PRINT_SIZE("int16_t", sizeof(int16_t));
+  PRINT_SIZE("int16_t*", sizeof(int16_t*));
   PRINT_SIZE("int32_t", sizeof(int32_t));
+  PRINT_SIZE("int32_t*", sizeof(int32_t*));
   PRINT_SIZE("int64_t", sizeof(int64_t));
+  PRINT_SIZE("int64_t*", sizeof(int64_t*));
 
   PRINT_SIZE("int_fast8_t", sizeof(int_fast8_t));
+  PRINT_SIZE("int_fast8_t*", sizeof(int_fast8_t*));
   PRINT_SIZE("int_fast16_t", sizeof(int_fast16_t));
+  PRINT_SIZE("int_fast16_t*", sizeof(int_fast16_t*));
   PRINT_SIZE("int_fast32_t", sizeof(int_fast32_t));
+  PRINT_SIZE("int_fast32_t*", sizeof(int_fast32_t*));
   PRINT_SIZE("int_fast64_t", sizeof(int_fast64_t));
+  PRINT_SIZE("int_fast64_t*", sizeof(int_fast64_t*));
 
   PRINT_SIZE("uint8_t", sizeof(uint8_t));
+  PRINT_SIZE("uint8_t*", sizeof(uint8_t*));
   PRINT_SIZE("uint16_t", sizeof(uint16_t));
+  PRINT_SIZE("uint16_t*", sizeof(uint16_t*));
   PRINT_SIZE("uint32_t", sizeof(uint32_t));
+  PRINT_SIZE("uint32_t*", sizeof(uint32_t*));
   PRINT_SIZE("uint64_t", sizeof(uint64_t));
+  PRINT_SIZE("uint64_t*", sizeof(uint64_t*));
 
   PRINT_SIZE("uint_fast8_t", sizeof(uint_fast8_t));
+  PRINT_SIZE("uint_fast8_t*", sizeof(uint_fast8_t*));
   PRINT_SIZE("uint_fast16_t", sizeof(uint_fast16_t));
+  PRINT_SIZE("uint_fast16_t*", sizeof(uint_fast16_t*));
   PRINT_SIZE("uint_fast32_t", sizeof(uint_fast32_t));
+  PRINT_SIZE("uint_fast32_t*", sizeof(uint_fast32_t*));
   PRINT_SIZE("uint_fast64_t", sizeof(uint_fast64_t));
+  PRINT_SIZE("uint_fast64_t*", sizeof(uint_fast64_t*));
 
   // Alternatively, you can use stringification
   // (https://gcc.gnu.org/onlinedocs/cpp/Stringification.html) so that
@@ -62,6 +86,7 @@ int main() {
   you.id = 12345;
   you.year = 4;
   PRINT_SIZE("student", sizeof(you));
+  PRINT_SIZE("student*", sizeof(&you));
 
   // Array declaration. Use your macro to print the size of this.
   int x[5];

30673a48d17a7be1882ca13cf58838de82977234
Simon Aertssen <Simonaertssen@gmail.com>
2021-06-08 21:11:50 +0200

    Added a

--- Week 3 Vectorization/recitation3/example1.c
+++ Week 3 Vectorization/recitation3/example1.c
@@ -4,12 +4,13 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define SIZE (1L << 16)
+#define SIZE (1L << 32)
 
-void test(uint8_t * a,  uint8_t * b) {
-  uint64_t i;
+void test(uint8_t* restrict a, uint8_t* restrict b) {
+  a = __builtin_assume_aligned(a, 32);
+  b = __builtin_assume_aligned(b, 32);
 
-  for (i = 0; i < SIZE; i++) {
+  for (uint64_t i = 0; i < SIZE; i++) {
     a[i] += b[i];
   }
 }

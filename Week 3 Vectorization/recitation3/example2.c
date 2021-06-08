a67e733a2b7455e84f4411dc4b97eb6a672d61c2
Simon Aertssen <Simonaertssen@gmail.com>
2021-06-08 21:55:08 +0200

    Getting better at seeing the vectorisation

--- Week 3 Vectorization/recitation3/example2.c
+++ Week 3 Vectorization/recitation3/example2.c
@@ -6,14 +6,14 @@
 
 #define SIZE (1L << 16)
 
-void test(uint8_t * restrict a, uint8_t * restrict b) {
+void test(uint8_t* restrict a, uint8_t* restrict b) {
   uint64_t i;
 
-  uint8_t * x = __builtin_assume_aligned(a, 16);
-  uint8_t * y = __builtin_assume_aligned(b, 16);
+  a = __builtin_assume_aligned(a, 16);
+  b = __builtin_assume_aligned(b, 16);
 
   for (i = 0; i < SIZE; i++) {
     /* max() */
-    if (y[i] > x[i]) x[i] = y[i];
+    a[i] = (b[i] > a[i]) ? b[i] : a[i];
   }
 }

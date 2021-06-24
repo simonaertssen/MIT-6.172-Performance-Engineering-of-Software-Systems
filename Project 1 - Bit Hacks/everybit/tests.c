a77c4100e9c990b7ac8b4806e284492aea19ddd4
Simon Joris A Aertssen <simonaertssen@gmail.com>
2021-06-24 12:49:45 +0200

    fixed an error with the precompiler which would not allow VLAs

--- Project 1 - Bit Hacks/everybit/tests.c
+++ Project 1 - Bit Hacks/everybit/tests.c
@@ -40,6 +40,8 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
+#define FIB_SIZE 54
+
  // ******************************* Prototypes *******************************
 
  // Creates a new bit array in test_bitarray by parsing a string of 0s
@@ -265,7 +267,6 @@ void testutil_require_valid_input(const size_t bit_offset,
 }
 
 // Precomputed array of fibonacci numbers
-const int FIB_SIZE = 55;
 const double fibs[FIB_SIZE] = { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903, 2971215073, 4807526976, 7778742049, 12586269025, 20365011074, 32951280099, 53316291173, 86267571272 };
 
 int timed_rotation(const double time_limit_seconds) {

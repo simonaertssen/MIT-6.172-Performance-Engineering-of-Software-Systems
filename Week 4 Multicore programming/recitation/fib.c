8ab9c233a21faafcbcccc5bd7ee9b572c01f5356
Simon Aertssen <Simonaertssen@gmail.com>
2021-07-21 21:16:02 +0200

    Adding some new clauses to the code

--- Week 4 Multicore programming/recitation/fib.c
+++ Week 4 Multicore programming/recitation/fib.c
@@ -5,28 +5,29 @@
 
 
 int64_t fib(int64_t n) {
-    if (n < 2) {
-        return n;
-    }
-    else {
-        int64_t x, y;
-#pragma omp task shared(x, n)
-        x = fib(n - 1);
-#pragma omp task shared(y, n)
-        y = fib(n - 2);
+    if (n < 2) return n;
+    int64_t x, y;
+#pragma omp task shared(x)
+    x = fib(n - 1);
+#pragma omp task shared(y)
+    y = fib(n - 2);
 #pragma omp taskwait
-        return (x + y);
-    }
+    return (x + y);
 }
 
 int main(int argc, char* argv[]) {
     int64_t n = atoi(argv[1]);
-    int64_t result = fib(n);
 
     omp_set_num_threads(3);
-
     int nthreads = omp_get_num_threads();
     printf("N = %d", nthreads);
 
-    printf("Fibonacci of %" PRId64 " is %" PRId64 ".\n", n, result);
+#pragma omp parallel
+    {
+#pragma omp single
+        {
+            int64_t result = fib(n);
+            printf("Fibonacci of %" PRId64 " is %" PRId64 ".\n", n, result);
+        }
+    }
 }

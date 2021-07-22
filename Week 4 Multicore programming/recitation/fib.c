3abfcbfbdbd3b5113f93a0aa946f65cf6f03b835
Simon Aertssen <Simonaertssen@gmail.com>
2021-07-22 12:19:42 +0200

    Trying to get those tasks working but the program is actually super slow

--- Week 4 Multicore programming/recitation/fib.c
+++ Week 4 Multicore programming/recitation/fib.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <stdlib.h>
-#include <omp.h>
+
+#include "omp.h"
 
 
 int64_t fib(int64_t n) {
     if (n < 2) return n;
     int64_t x, y;
-#pragma omp task shared(x)
+#pragma omp task shared(x,n)
     x = fib(n - 1);
-#pragma omp task shared(y)
+#pragma omp task shared(y,n)
     y = fib(n - 2);
 #pragma omp taskwait
     return (x + y);
@@ -17,17 +18,18 @@ int64_t fib(int64_t n) {
 
 int main(int argc, char* argv[]) {
     int64_t n = atoi(argv[1]);
+    int64_t result;
 
-    omp_set_num_threads(3);
+    omp_set_num_threads(4);
     int nthreads = omp_get_num_threads();
-    printf("N = %d", nthreads);
+    printf("N = %d\n", nthreads);
 
 #pragma omp parallel
     {
-#pragma omp single
+#pragma omp master
         {
-            int64_t result = fib(n);
-            printf("Fibonacci of %" PRId64 " is %" PRId64 ".\n", n, result);
+            result = fib(n);
         }
     }
+    printf("Fibonacci of %" PRId64 " is %" PRId64 ".\n", n, result);
 }

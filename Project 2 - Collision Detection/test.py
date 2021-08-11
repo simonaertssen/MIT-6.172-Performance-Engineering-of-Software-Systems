fa2aad6d40de40d79dfd09a041aad274600b35db
Simon Aertssen <Simonaertssen@gmail.com>
2021-08-12 00:48:46 +0200

    Making a test suite that will automatically generate tables to test with and perform testing of our improved algorithms

--- Project 2 - Collision Detection/test.py
+++ Project 2 - Collision Detection/test.py
@@ -2,13 +2,20 @@ import os
 import sys
 
 
-def produce_test_table():
-    for filename in os.listdir('screensaver/input'):
+def produce_test_table(frames):
+    output_filename = f'test_table_{frames}.txt'
+    output_file = open(output_filename, "w")
+    
+    output = ''
+    for filename in os.listdir('../screensaver/input/'):
         print(filename)
+        output += f'{filename} | | | |'
+
+    output_file.write(output)
 
 
 def main(argv):
-    produce_test_table()   
+    produce_test_table(10)   
 
 
 if __name__ == '__main__':
